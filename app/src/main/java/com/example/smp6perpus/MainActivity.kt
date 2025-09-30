faaa1077cca6aa3bc2e0f8b444dd83774c35266d
awangga01 <alfianawangga15@gmail.com>
2025-10-01 01:09:47 +0900

    init splash screen

--- app/src/main/java/com/example/smp6perpus/MainActivity.kt
+++ app/src/main/java/com/example/smp6perpus/MainActivity.kt
@@ -3,18 +3,24 @@ package com.example.smp6perpus
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.smp6perpus.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        splashScreen()
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    //install splash screen
+    private fun splashScreen() {
+        Thread.sleep(1000)
+        installSplashScreen()
     }
 }
